4242920e3dedf34d385d788ed25eefb8ce579cdd
AndreiHromau <Andreihromau@gmail.com>
2026-04-29 16:28:37 +0300

    feat: Добавил каркас проекта на Spring Boot

---
+++ src/main/java/ru/aston/astonapigateway/AstonApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.aston.astonapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/aston/astonapigateway/AstonApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.astonapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
